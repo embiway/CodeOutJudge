2e1d6a2f8db779fe59d37a5487748ae898856ec0
embiway <mridulbhatt0607@gmail.com>
2020-05-26 20:43:20 +0530

    Added models in django admin

--- CodeOutJudge/Problems/admin.py
+++ CodeOutJudge/Problems/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Problem , Profile , Submission , InputFile , OutputFile
 
 # Register your models here.
+
+admin.site.register(Profile )
+admin.site.register(Problem)
+admin.site.register(Submission)
+admin.site.register(InputFile)
+admin.site.register(OutputFile)
--- CodeOutJudge/Problems/models.py
+++ CodeOutJudge/Problems/models.py
@@ -2,12 +2,38 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class OutputFile(models.Model):
+    file_content = models.FileField()
+    def __str__(self):
+        return self.id
+
+class InputFile(models.Model):
+    file_content = models.FileField()
+    output_file = models.OneToOneField(OutputFile , on_delete=models.CASCADE)
+    def __str__(self):
+        return self.id
+
+class Submission(models.Model):
+    submission = models.FileField()
+    def __str__(self):
+        return self.id
+
+class Problem(models.Model):
+    problem_statement = models.TextField(max_length = 10000, blank = False)
+    input_file = models.ForeignKey(InputFile , on_delete=models.CASCADE)
+    output_file = models.ForeignKey(OutputFile , on_delete=models.CASCADE)
+    def __str__(self):
+        return self.id
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
+    problems = models.ForeignKey(Problem , on_delete=models.CASCADE , null=True)
+    submissions = models.ForeignKey(Submission , on_delete=models.CASCADE , null=True)
 
-# class Problem(models.Model):
-#     problem_statement = models.TextField(max_length = 10000, blank = False)
-#     input_file = models.FileField
+    def __str__(self):
+        return self.user.username
+
